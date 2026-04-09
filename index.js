20ffc351de7b5f3bcaa973ea723daea30885cfbe
masaki0723a <masaki0723a@gmail.com>
2026-04-09 18:50:05 +0700

    start

---
+++ index.js
@@ -0,0 +1,18 @@
+cat << 'EOF' > index.js
+require('dotenv').config();
+
+const { Client, GatewayIntentBits, Events } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates
+  ],
+});
+
+client.once(Events.ClientReady, () => {
+  console.log("起動:", client.user.tag);
+});
+
+client.login(process.env.TOKEN);
+EOF
